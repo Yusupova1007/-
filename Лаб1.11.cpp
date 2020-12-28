7b6a07bc7de9866df45bcdaebfad053e71d17941
Yusupova1007 <76661552+Yusupova1007@users.noreply.github.com>
2020-12-28 16:28:30 +0500

    Add files via upload

---
+++ Лаб1.11.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float a, b, c, d, e, f, g, i;
+	a = 1000;
+	b = 0.0001;
+	//Вычисление числителя
+	c = pow((a - b), 3);
+	d = pow(a, 3);
+	e = 3 * a * (pow(b, 2));
+	//Вычисление знаменателя
+	f = pow(b, 3);
+	g = 3 * (pow(a, 2)) * b;
+	//Значение выражения
+	i = (c - (d - e)) / f - g;
+	cout << "Результат выражения при float=" << i << endl;
+}
